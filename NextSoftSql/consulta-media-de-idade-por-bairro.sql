e95316858626bf162a3ba68e274c045a2f774fc3
Rafaela Montalvão <96311809+rafaelainocencio@users.noreply.github.com>
2022-05-27 20:41:34 -0300

    Update consulta-media-de-idade-por-bairro.sql

--- NextSoftSql/consulta-media-de-idade-por-bairro.sql
+++ NextSoftSql/consulta-media-de-idade-por-bairro.sql
@@ -1,6 +1,7 @@
+USE ConjuntoHabitacional;
 GO
 
-SELECT C.Bairro, AVG(M.Idade) AS 'Média de Idade'
+SELECT C.Bairro, AVG(M.Idade) AS 'MÃ©dia de Idade'
 FROM Condominio AS C
 INNER JOIN Familia AS F ON C.Id = F.Id_Condominio 
 INNER JOIN Morador AS M ON F.Id = M.Id_Familia
